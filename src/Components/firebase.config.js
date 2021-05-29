34b29a59aba0ab791d68dbc7003d93d19b8f76cd
Md.Mohtasim Shariar <onim04@gmail.com>
2021-05-29 19:18:10 +0600

    ok

---
+++ src/Components/firebase.config.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAANfFb_1boVVgxZXOB8vXIAr_oUM6i9Xk",
+    authDomain: "hello-buddy-media.firebaseapp.com",
+    projectId: "hello-buddy-media",
+    storageBucket: "hello-buddy-media.appspot.com",
+    messagingSenderId: "585484110222",
+    appId: "1:585484110222:web:7a7f6ef43d5099a15b7cdf",
+    measurementId: "G-B54N57903F"
+};
+
+export default firebaseConfig;
+
+
+//   databaseURL: "https://solar-studio-d83bc.firebaseio.com",
